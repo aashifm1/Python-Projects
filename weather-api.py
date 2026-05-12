374fafd067d5464d3ffde6dfb84895e255fccb50
Aashif M <Aashif8056@gmail.com>
2026-05-12 12:24:49 +0530

    weather-api

---
+++ weather-api.py
@@ -0,0 +1,45 @@
+
+# Install dependencies: requests, python-dotenv
+
+# Import Libraries
+import requests
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY") # create a .env file and add your API_KEY in the file
+
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+
+def get_weather(city):
+    params = {
+        "q": city,
+        "appid": API_KEY,
+        "units": "metric"
+    }
+
+    response = requests.get(BASE_URL, params=params)
+
+    if response.status_code != 200:
+        print("Error fetching weather data")
+        return
+
+    data = response.json()
+
+    city_name = data["name"]
+    temp = data["main"]["temp"]
+    humidity = data["main"]["humidity"]
+    description = data["weather"][0]["description"]
+
+    print(f"\nWeather in {city_name}")
+    print(f"Temperature: {temp}°C")
+    print(f"Humidity: {humidity}%")
+    print(f"Condition: {description}")
+
+
+if __name__ == "__main__":
+    city = input("Enter city name: ")
+    get_weather(city)
